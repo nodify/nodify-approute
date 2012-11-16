36ff352549f8f78a0783910d538894b1518a086d
Meadhbh Hamrick <OhMeadhbh@gmail.com>
2012-11-16 12:52:50 -0800

    * added parsed URL to params for all invocations * changed indentation * incremented rev number

--- example/simpleExample.js
+++ example/simpleExample.js
@@ -1,14 +1,14 @@
 try {
-    var connect = require( 'connect' );
-    var props   = require( 'node-props' );
-    var approute  = require( '../nodify-approute.js' );
+  var connect = require( 'connect' );
+  var props   = require( 'node-props' );
+  var approute  = require( '../nodify-approute.js' );
 } catch( e ) {
-    console.log( "Error while requiring packages. Execute these commands to install them:" );
-    console.log( "  npm install connect" );
-    console.log( "  npm install node-props" );
-    console.log( "Then execute this command to start the example server:" );
-    console.log( "  node simpleExample.js --config file://exampleProperties.json" );
-    process.exit( 1 );
+  console.log( "Error while requiring packages. Execute these commands to install them:" );
+  console.log( "  npm install connect" );
+  console.log( "  npm install node-props" );
+  console.log( "Then execute this command to start the example server:" );
+  console.log( "  node simpleExample.js --config file://exampleProperties.json" );
+  process.exit( 1 );
 }
 
 var g;
@@ -17,58 +17,59 @@ var exterior = "/app";
 var generator;
 
 var route_descriptor = {
-    routes: [
-	{
-	    route: "/",
-	    get: function( body, params, callback ) {
-		callback( "hello!" );
-	    }
-	},
-	{
-	    route: "/simple/([0-9]{3})",
-	    params: [ "id" ],
-	    get: function( body, params, callback ) {
-		callback( {success: true, id: params.id } );
-	    },
-	    post: function( body, params, callback ) {
-		console.log( body );
-		console.log( params );
-		callback( {success:true, id: params.id, appid: body.appid} );
-	    }
-	}
-    ]
+  headers: [ 'host' ],
+  routes: [
+    {
+      route: "/",
+      get: function( body, params, callback ) {
+	callback( "hello!" );
+      }
+    },
+    {
+      route: "/simple/([0-9]{3})",
+      params: [ "id" ],
+      get: function( body, params, callback ) {
+	callback( {success: true, id: params.id } );
+      },
+      post: function( body, params, callback ) {
+	console.log( body );
+	console.log( params );
+	callback( {success:true, id: params.id, appid: body.appid} );
+      }
+    }
+  ]
 };
 
 props.read( post_read );
 
 function post_read( properties ) {
-    g = properties;
+  g = properties;
 
-    if( g.exterior ) {
-	route_descriptor.exterior = g.exterior + exterior;
-    }
+  if( g.exterior ) {
+    route_descriptor.exterior = g.exterior + exterior;
+  }
 
-    server = connect();
+  server = connect();
 
-    server.use( connect.favicon( g.favicon ) );
+  server.use( connect.favicon( g.favicon ) );
 
-    server.use( connect.logger( g.logger ) );
+  server.use( connect.logger( g.logger ) );
 
-    var router = approute.createInstance( route_descriptor );
+  var router = approute.createInstance( route_descriptor );
 
-    router.init( post_router_init );
+  router.init( post_router_init );
 }
 
 function post_router_init ( middleware_function, router_instance ) {
-    server.use( "/app", middleware_function );
+  server.use( "/app", middleware_function );
 
-    if( g.static && g.static.path ) {
-	server.use( connect.static( __dirname + g.static.path, g.static.options ) );
-    }
+  if( g.static && g.static.path ) {
+    server.use( connect.static( __dirname + g.static.path, g.static.options ) );
+  }
 
-    server.use( connect.errorHandler );
+  server.use( connect.errorHandler );
 
-    if( g.listen && g.listen.port ) {
-	server.listen( g.listen.port, g.listen.host );
-    }
+  if( g.listen && g.listen.port ) {
+    server.listen( g.listen.port, g.listen.host );
+  }
 }
--- nodify-approute.js
+++ nodify-approute.js
@@ -1,110 +1,113 @@
 ( function ( ) {
-    function approute( options ) {
-	this.options = options;
-	this.routes = [];
+  var url = require( 'url' );
+
+  function approute( options ) {
+    this.options = options;
+    this.routes = [];
+  }
+
+  approute.prototype.init = function ( callback ) {
+    var that = this;
+
+    for( var i = 0, il = this.options.routes.length; i < il; i++ ) {
+      this.routes.push(  new RegExp( '^' + this.options.routes[i].route + '$' ) );
     }
 
-    approute.prototype.init = function ( callback ) {
-	var that = this;
+    function _approute_function ( request, response, next ) {
+      var current, paramblock, body, params;
 
-	for( var i = 0, il = this.options.routes.length; i < il; i++ ) {
-	    this.routes.push(  new RegExp( '^' + this.options.routes[i].route + '$' ) );
+      params = { url: url.parse( 'http://' + request.headers.host + request.url ) };
+      delete( params.url.protocol );
+      delete( params.url.href );
+
+      for( var i = 0, il = that.routes.length; i < il; i++ ) {
+	paramblock = that.routes[ i ].exec( params.url.pathname );
+	if( paramblock ) {
+	  current = that.options.routes[ i ];
+	  break;
+	}
+      }
+
+      if( ! current ) {
+	response.statusCode = 404;
+	return( next( 404 ) );
+      }
+
+      var method = request.method.toLowerCase();
+      if( 'function' !== typeof current[ method ] ) {
+	response.statusCode = 405;
+	return( next( 405 ) );
+      }
+
+      var content_type = request.headers[ 'content-type' ];
+      if( content_type && ( 'application/json' !== content_type ) ) {
+	response.statusCode = 415;
+	return( next( 415 ) );
+      }
+
+      if( paramblock.length > 1 && current.params ) {
+        for( var i = 0, il = paramblock.length - 1, pl = current.params.length; (i < il) && (i < pl); i++ ) {
+	  params[ current.params[ i ] ] = paramblock[ i + 1 ];
+        }
+      }
+
+      if( 'function' === typeof current.validate ) {
+	return( current.validate( params, _do_body ) );
+      } else {
+	_do_body( true );
+      }
+
+      function _do_body ( okay ) {
+	if( false === okay ) {
+	  response.statusCode = 404;
+	  return( next( "404 Not Found" ) );
 	}
 
-	function _approute_function ( request, response, next ) {
-	    var current, paramblock, body, params;
+	body = "";
+	request.on( 'data', function ( chunk ) {
+	  body += chunk;
+	} );
 
-	    for( var i = 0, il = that.routes.length; i < il; i++ ) {
-		paramblock = that.routes[ i ].exec( request.url );
-		if( paramblock ) {
-		    current = that.options.routes[ i ];
-		    break;
-		}
-	    }
+	request.on( 'end', function () {
+	  var parsed_body;
 
-	    if( ! current ) {
-		response.statusCode = 404;
-		return( next( "404 Not Found" ) );
-	    }
+	  try {
+	    parsed_body = JSON.parse( body );
+	  } catch( e ) {
+	    parsed_body = {};
+	  }
 
-	    var method = request.method.toLowerCase();
-	    if( 'function' !== typeof current[ method ] ) {
-		response.statusCode = 405;
-		return( next( "405 Method Not Allowed" ) );
-	    }
+	  current[ method ]( parsed_body, params, function( _r ) {
+	    var data;
+	    var content_type;
 
-	    var content_type = request.headers[ 'content-type' ];
-	    if( content_type && ( 'application/json' !== content_type ) ) {
-		response.statusCode = 415;
-		return( next( "415 Unsupported Media Type" ) );
-	    }
-
-	    if( ( paramblock.length > 1 ) && current.params ) {
-		params = {};
-		for( var i = 0, il = paramblock.length - 1, pl = current.params.length; (i < il) && (i < pl); i++ ) {
-		    params[ current.params[ i ] ] = paramblock[ i + 1 ];
-		}
-
-		if( 'function' === typeof current.validate ) {
-		    return( current.validate( params, _do_body ) );
-		} else {
-		    _do_body( true );
-		}
+	    if( 'string' === typeof _r ) {
+	      data = _r;
+	      content_type = 'text/plain';
 	    } else {
-		return( _do_body( true ) );
+	      data = JSON.stringify( _r );
+	      content_type = 'application/json';
 	    }
 
-	    function _do_body ( okay ) {
-		if( false === okay ) {
-		    response.statusCode = 404;
-		    return( next( "404 Not Found" ) );
-		}
-
-		body = "";
-		request.on( 'data', function ( chunk ) {
-		    body += chunk;
-		} );
-
-		request.on( 'end', function () {
-		    var parsed_body;
-
-		    try {
-			parsed_body = JSON.parse( body );
-		    } catch( e ) {
-			parsed_body = {};
-		    }
-
-		    current[ method ]( parsed_body, params, function( _r ) {
-			var data;
-			var content_type;
-
-			if( 'string' === typeof _r ) {
-			    data = _r;
-			    content_type = 'text/plain';
-			} else {
-			    data = JSON.stringify( _r );
-			    content_type = 'application/json';
-			}
-
-			var headers = {};
-		    
-			if( data.length > 0 ) {
-			    headers[ 'Content-Type' ] = content_type;
-			    headers[ 'Content-Length' ] = data.length;
-			}
-
-			response.writeHead( 200, headers );
-			response.end( data );
-		    } );
-		
-		} );
+	    var headers = {};
+	    
+	    if( data.length > 0 ) {
+	      headers[ 'Content-Type' ] = content_type;
+	      headers[ 'Content-Length' ] = data.length;
 	    }
-	}
-	callback( _approute_function, this );
+
+	    response.writeHead( 200, headers );
+	    response.end( data );
+	  } );
+	  
+	} );
+      }
     }
+    callback( _approute_function, this );
+  }
 
-    exports.createInstance = function ( options ) {
-	return( new approute( options ) );
-    };
+  exports.createInstance = function ( options ) {
+    return( new approute( options ) );
+  };
 
 } )();
